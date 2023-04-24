b74234c88d5b493d908ac07bbf51170759b3a2f9
TarasKing <ivanovtaras@live.ru>
2023-04-23 20:00:58 -0700

    added read more

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import Tours from "./Tours";
+import Loading from "./Loading";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   const fetchTours = async () => {
     setIsLoading(true);
@@ -14,7 +19,7 @@ function App() {
       const tours = await response.json();
       setIsLoading(false);
       setTours(tours);
-      console.log(tours);
+      // console.log(tours);
     } catch (error) {
       setIsLoading(false);
       console.log(error);
@@ -28,14 +33,14 @@ function App() {
   if (isLoading) {
     return (
       <main>
-        <h1>Loading...</h1>
+        <Loading />
       </main>
     );
   }
   return (
-    <>
-      <Tours />
-    </>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,10 +1,18 @@
-// import React from "react";
-
-const Tours = () => {
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
   return (
-    <div>
-      <h2>Tours</h2>
-    </div>
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
   );
 };
 
