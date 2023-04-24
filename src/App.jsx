7d610c73539be51e294293017a13437b2285cd09
TarasKing <ivanovtaras@live.ru>
2023-04-23 19:26:51 -0700

    Tours react project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setIsLoading(false);
+      setTours(tours);
+      console.log(tours);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+  return (
+    <>
+      <Tours />
+    </>
+  );
+}
+
+export default App;
